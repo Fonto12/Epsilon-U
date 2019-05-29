05bae4d48306e7412775221e4ca983a4e44d8ee2
Fonto12 <35042528+Fonto12@users.noreply.github.com>
2019-05-29 14:28:42 +0200

    Add files via upload

---
+++ 20%/shell/four.sh
@@ -0,0 +1,4 @@
+
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Instituto/Desktop/20%/fondos/4.jpg"'
+exit
+
---
+++ 20%/shell/twelve.sh
@@ -0,0 +1,4 @@
+
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Instituto/Desktop/20%/fondos/12.jpg"'
+exit
+
